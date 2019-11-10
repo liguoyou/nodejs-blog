74ad9fe3fce6802a5269e356b167f629a195a67f
GuoYou.Li <516115887@qq.com>
2019-11-10 17:43:49 +0800

    readme.md

--- README.md
+++ README.md
@@ -3,13 +3,20 @@
 - 实现增删改查博客数据
 
 - 利用 cookie/session/redis 保存登录状态
+  
+- 密码加密
 
 - nginx 反向代理
 
 - 日志记录
 
-- 安全处理
+- 安全处理: 防止 xss注入, sql 注入
 
-博客前端测试地址: [nodejs-blog-html](https://github.com/liguoyou/nodejs-blog-html)
+博客前端代码: [nodejs-blog-html](https://github.com/liguoyou/nodejs-blog-html)
+
+### 其他版本
+
+- [express 重构版](https://github.com/liguoyou/nodejs-express-blog)
+- [koa2 重构版](https://github.com/liguoyou/nodejs-koa2-blog)
 
 参考/来源: [前端晋升全栈工程师必备课程 Node.js 从零开发 web server 博客项目](https://coding.imooc.com/class/320.html)
--- src/router/blog.js
+++ src/router/blog.js
@@ -1,117 +1,117 @@
 const {
-	getList,
-	getDetail,
-	newBlog,
-	updateBlog,
-	delBlog
+  getList,
+  getDetail,
+  newBlog,
+  updateBlog,
+  delBlog
 } = require('../controller/blog.js')
 const { SuccessModel, ErrorModel } = require('../model/resData.js')
 
 // 登录验证函数
 const loginCheck = req => {
-	if (!req.session.username) {
-		return Promise.resolve(new ErrorModel('尚未登录'))
-	}
+  if (!req.session.username) {
+    return Promise.resolve(new ErrorModel('尚未登录'))
+  }
 }
 
 const handleBlogRouter = (req, res) => {
-	const method = req.method // GET POST
-	const path = req.path
-	const query = req.query
-	const id = req.query.id
-	const body = req.body
+  const method = req.method // GET POST
+  const path = req.path
+  const query = req.query
+  const id = req.query.id
+  const body = req.body
 
-	// 校验登录结果
-	const loginCheckResult = loginCheck(req)
+  // 校验登录结果
+  const loginCheckResult = loginCheck(req)
 
-	// 获取博客列表
-	if (method === 'GET' && path === '/api/blog/list') {
-		let author = query.author || ''
-		const keyword = query.keyword || ''
+  // 获取博客列表
+  if (method === 'GET' && path === '/api/blog/list') {
+    let author = query.author || ''
+    const keyword = query.keyword || ''
 
-		// 管理员界面
-		if (req.query.isadmin) {
-			// 登录权限验证
-			if (loginCheckResult) return loginCheckResult
+    // 管理员界面
+    if (req.query.isadmin) {
+      // 登录权限验证
+      if (loginCheckResult) return loginCheckResult
 
-			// 强制查询自己的博客
-			author = req.session.username
-		}
+      // 强制查询自己的博客
+      author = req.session.username
+    }
 
-		const result = getList(author, keyword)
-		return result.then(listData => {
-			return new SuccessModel(listData)
-		})
-	}
+    const result = getList(author, keyword)
+    return result.then(listData => {
+      return new SuccessModel(listData)
+    })
+  }
 
-	// 获取博客详情
-	if (method === 'GET' && path === '/api/blog/detail') {
-		if (!id) {
-			return new ErrorModel('参数不能为空!')
-		}
+  // 获取博客详情
+  if (method === 'GET' && path === '/api/blog/detail') {
+    if (!id) {
+      return new ErrorModel('参数不能为空!')
+    }
 
-		const result = getDetail(id)
-		return result.then(data => {
-			if (data[0]) {
-				return new SuccessModel(data[0])
-			}
-			return new ErrorModel('未查询到数据')
-		})
-	}
+    const result = getDetail(id)
+    return result.then(data => {
+      if (data[0]) {
+        return new SuccessModel(data[0])
+      }
+      return new ErrorModel('未查询到数据')
+    })
+  }
 
-	// 新增一篇博客
-	if (method === 'POST' && path === '/api/blog/new') {
-		// 登录权限验证
-		if (loginCheckResult) return loginCheckResult
+  // 新增一篇博客
+  if (method === 'POST' && path === '/api/blog/new') {
+    // 登录权限验证
+    if (loginCheckResult) return loginCheckResult
 
-		body.author = req.session.username
-		const result = newBlog(body)
-		return result.then(insertData => {
-			if (insertData.id) {
-				return new SuccessModel(insertData)
-			}
-			return new ErrorModel('新增失败!')
-		})
-	}
+    body.author = req.session.username
+    const result = newBlog(body)
+    return result.then(insertData => {
+      if (insertData.id) {
+        return new SuccessModel(insertData)
+      }
+      return new ErrorModel('新增失败!')
+    })
+  }
 
-	// 更新一篇博客
-	if (method === 'POST' && path === '/api/blog/update') {
-		// 登录权限验证
-		if (loginCheckResult) return loginCheckResult
+  // 更新一篇博客
+  if (method === 'POST' && path === '/api/blog/update') {
+    // 登录权限验证
+    if (loginCheckResult) return loginCheckResult
 
-		if (!id || !body) {
-			return new ErrorModel('参数不能为空!')
-		}
+    if (!id || !body) {
+      return new ErrorModel('参数不能为空!')
+    }
 
-		const author = req.session.username
+    const author = req.session.username
 
-		const result = updateBlog(id, body, author)
-		return result.then(res => {
-			if (res) {
-				return new SuccessModel('更新成功!')
-			}
-			return new ErrorModel('更新失败!')
-		})
-	}
+    const result = updateBlog(id, body, author)
+    return result.then(res => {
+      if (res) {
+        return new SuccessModel('更新成功!')
+      }
+      return new ErrorModel('更新失败!')
+    })
+  }
 
-	// 删除博客
-	if (method === 'POST' && req.path === '/api/blog/del') {
-		// 登录权限验证
-		if (loginCheckResult) return loginCheckResult
+  // 删除博客
+  if (method === 'DELETE' && req.path === '/api/blog/del') {
+    // 登录权限验证
+    if (loginCheckResult) return loginCheckResult
 
-		if (!id) {
-			return new ErrorModel('参数不能为空!')
-		}
+    if (!id) {
+      return new ErrorModel('参数不能为空!')
+    }
 
-		const author = req.session.username
-		const result = delBlog(id, author)
-		return result.then(res => {
-			if (res) {
-				return new SuccessModel('删除成功!')
-			}
-			return new ErrorModel('删除失败!')
-		})
-	}
+    const author = req.session.username
+    const result = delBlog(id, author)
+    return result.then(res => {
+      if (res) {
+        return new SuccessModel('删除成功!')
+      }
+      return new ErrorModel('删除失败!')
+    })
+  }
 }
 
 module.exports = handleBlogRouter
